e506f3e4ef0d4ceb743d4a255ba49327bcbde9e2
Necis <defigueiredo.anthony@gmail.com>
2020-12-17 12:19:57 +0100

    Achieve step One

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListImagesComponent } from './list-images/list-images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListImagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-images/list-images.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-images',
+  templateUrl: './list-images.component.html',
+  styleUrls: ['./list-images.component.css']
+})
+export class ListImagesComponent implements OnInit {
+  @Input()
+  images: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
