88f77ac357a6dce2121d30b0995dd398e5bd828b
Necis <defigueiredo.anthony@gmail.com>
2020-12-17 15:35:02 +0100

    achieve step 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,18 @@ import {GetMarsImagesService} from './get-mars-images.service';
 })
 export class AppComponent {
   title = 'MarsGallery';
-  marsPhotos = new GetMarsImagesService();
+  marsPhotos : any = [];
+
+  constructor(private marsImagesService : GetMarsImagesService) { }
+
+  ngOnInit() {
+    this.marsImagesService.GetMarsImagesFromApi().subscribe(
+      (response) => {
+        this.marsPhotos = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
 }
