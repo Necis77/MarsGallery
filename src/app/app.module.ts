21f311b36d810b058689c17d9cac048a32080b31
Necis <defigueiredo.anthony@gmail.com>
2020-12-17 12:41:37 +0100

    Try to achieve step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListImagesComponent } from './list-images/list-images.component';
+import { ListImagesItemComponent } from './list-images-item/list-images-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListImagesComponent
+    ListImagesComponent,
+    ListImagesItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-images-item/list-images-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-images-item',
+  templateUrl: './list-images-item.component.html',
+  styleUrls: ['./list-images-item.component.css']
+})
+export class ListImagesItemComponent implements OnInit {
+
+  @Input()
+  marsImage: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
