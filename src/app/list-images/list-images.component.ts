6d1fe10d9668a5001b4932f5f82561a0cbee2d09
Backu <lucasjuillard12@gmail.com>
2020-12-17 17:02:32 +0100

    achieve step 6

--- src/app/list-images/list-images.component.ts
+++ src/app/list-images/list-images.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {GetMarsImagesService} from "../get-mars-images.service";
 
 @Component({
   selector: 'app-list-images',
@@ -8,9 +9,27 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ListImagesComponent implements OnInit {
   @Input()
   images: any;
-  constructor() { }
+  Day;
+  Month;
+  Year;
+
+  errorCatch = false;
+  constructor(private marsImagesService: GetMarsImagesService) { }
 
   ngOnInit(): void {
+  }
+  refreshImages() {
+    this.marsImagesService.GetMarsImagesFromApi(this.Day,this.Month,this.Year).subscribe(
+      (response) => {
+        this.errorCatch = false;
+        this.images = response.photos;
+      },
+      (error) => {
+        this.errorCatch = true;
+        console.log('toto' + error);
+        this.images = null;
+      }
+    );
   }
 
 }
